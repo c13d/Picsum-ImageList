2db2a7f5cb3a08fde5495be32449d4d40f9a4b50
davin <christophorusdavin@gmail.com>
2023-02-02 13:10:21 +0700

    Update image table view cell

--- Picsum-ImageList/ImageTableViewCell.swift
+++ Picsum-ImageList/ImageTableViewCell.swift
@@ -14,8 +14,13 @@ class ImageTableViewCell: UITableViewCell{
     static let reuseID = "ImageTableViewCell"
     static let rowHeight: CGFloat = 100
     
+    let stackView = UIStackView()
+    
     let authorTitleLabel = UILabel()
     let authorLabel = UILabel()
+    let imageImageView = UIImageView()
+    
+    let imageSize = rowHeight
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -35,32 +40,49 @@ class ImageTableViewCell: UITableViewCell{
     
     func configureCell(){
         authorLabel.text = "Any Name"
+        imageImageView.image = UIImage(named: "sampleImage")
     }
 }
 
 extension ImageTableViewCell{
     
     private func setup(){
-        authorTitleLabel.text = "Author:"
-        authorLabel.text = ""
         
+        stackView.axis = .vertical
+        stackView.spacing = 4
+        stackView.alignment = .fill
+        
+        authorTitleLabel.text = "Author:"
+        authorTitleLabel.textAlignment = .center
+        authorTitleLabel.font = UIFont.preferredFont(for: .title3, weight: .bold)
+        
+        authorLabel.textAlignment = .center
+        authorLabel.numberOfLines = 0
+        authorLabel.font = UIFont.preferredFont(for: .title3, weight: .bold)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        imageImageView.translatesAutoresizingMaskIntoConstraints = false
         authorTitleLabel.translatesAutoresizingMaskIntoConstraints = false
         authorLabel.translatesAutoresizingMaskIntoConstraints = false
         
-        addSubview(authorTitleLabel)
-        addSubview(authorLabel)
+        addSubview(imageImageView)
+        addSubview(stackView)
+        
+        stackView.addArrangedSubview(authorTitleLabel)
+        stackView.addArrangedSubview(authorLabel)
     }
     
     private func layout(){
         
         NSLayoutConstraint.activate([
-            authorTitleLabel.topAnchor.constraint(equalTo: topAnchor),
-            authorTitleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
-            authorTitleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            imageImageView.topAnchor.constraint(equalTo: topAnchor),
+            imageImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageImageView.heightAnchor.constraint(equalToConstant: imageSize),
+            imageImageView.widthAnchor.constraint(equalToConstant: imageSize),
             
-            authorLabel.topAnchor.constraint(equalTo: authorLabel.bottomAnchor),
-            authorLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
-            authorLabel.trailingAnchor.constraint(equalTo: trailingAnchor)
+            stackView.leadingAnchor.constraint(equalTo: imageImageView.trailingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            stackView.centerYAnchor.constraint(equalTo: centerYAnchor),
         ])
         
     }
---
+++ Picsum-ImageList/UIFont+Utils.swift
@@ -0,0 +1,17 @@
+//
+//  UIFont+Utils.swift
+//  Picsum-ImageList
+//
+//  Created by Christophorus Davin on 02/02/23.
+//
+
+import UIKit
+
+extension UIFont {
+    static func preferredFont(for style: TextStyle, weight: Weight) -> UIFont {
+        let metrics = UIFontMetrics(forTextStyle: style)
+        let desc = UIFontDescriptor.preferredFontDescriptor(withTextStyle: style)
+        let font = UIFont.systemFont(ofSize: desc.pointSize, weight: weight)
+        return metrics.scaledFont(for: font)
+    }
+}
