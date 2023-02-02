122936e2d51622b45555df02913f5e14298eb887
davin <christophorusdavin@gmail.com>
2023-02-02 15:09:09 +0700

    add image view to image detail view

--- Picsum-ImageList/ImageDetailViewController.swift
+++ Picsum-ImageList/ImageDetailViewController.swift
@@ -9,17 +9,42 @@ import UIKit
 
 class ImageDetailViewController: UIViewController{
     
+    let imageImageView = UIImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
+        layout()
     }
     func bind(model: String){
-        
         print(model)
+        
+        imageImageView.image = UIImage(named: "sampleImage")
     }
     
+    
+}
+
+extension ImageDetailViewController{
     func setup(){
         navigationItem.title = "Image Detail"
         view.backgroundColor = .systemBackground
+        
+        imageImageView.contentMode = .scaleToFill
+        
+        imageImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(imageImageView)
     }
+    
+    func layout(){
+        
+        NSLayoutConstraint.activate([
+            imageImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            imageImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            imageImageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            imageImageView.heightAnchor.constraint(equalToConstant: 220)
+        ])
+    }
+    
 }
